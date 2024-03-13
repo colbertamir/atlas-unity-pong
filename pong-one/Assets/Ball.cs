b3266f787231fde98c6dfd0df7e5240b572e8513
colbertamir <colbertamir1516@gmail.com>
2024-03-13 16:48:01 -0500

    new stuff :)

--- pong-one/Assets/Ball.cs
+++ pong-one/Assets/Ball.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 5f; // Adjust this value to change the speed of the ball
+    private Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        LaunchBall();
     }
 
-    // Update is called once per frame
-    void Update()
+    // Launch the ball in a random direction
+    void LaunchBall()
     {
-        
+        float randomAngle = Random.Range(45f, 135f); // Random angle between 45 and 135 degrees
+        Vector2 launchDirection = Quaternion.Euler(0, 0, randomAngle) * Vector2.right; // Convert angle to vector
+        rb.velocity = launchDirection * speed;
+    }
+
+    // Handle collisions
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Paddle"))
+        {
+            // Reverse the direction of the ball when it collides with a paddle
+            Vector2 reflection = Vector2.Reflect(rb.velocity, collision.contacts[0].normal).normalized;
+            rb.velocity = reflection * speed;
+        }
+    }
+
+    // Reset ball position and velocity
+    public void ResetBall()
+    {
+        rb.velocity = Vector2.zero;
+        transform.position = Vector2.zero;
+        LaunchBall();
     }
 }
---
+++ pong-one/Assets/Player.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public KeyCode upKey; // Define the upKey field
+    public KeyCode downKey; // Define the downKey field
+
+}
