e41dfcbd2cbc9d3be41b2b351c4f9f13818d0e9d
colbertamir <colbertamir1516@gmail.com>
2024-03-13 20:28:18 -0500

    player 2

--- pong-one/Assets/Paddle.cs
+++ pong-one/Assets/Paddle.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed; // Speed of paddle
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Move the paddle up when the 'W' key or up arrow is pressed
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
+
+        // Move the paddle down when the 'S' key or down arrow is pressed
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector2.down * speed * Time.deltaTime);
+        }
     }
 }
